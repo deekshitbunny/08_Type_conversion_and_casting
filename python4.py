4f4563fa7a2a9f763e23a8e9745d2e43854b7f5e
Deekshitth Vegi <65802453+deekshitbunny@users.noreply.github.com>
2022-06-28 17:15:56 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,21 @@
+c=6 ; d='intelli' ; e='gent'
+print(d+str(c)+e)
+
+f = 'iphone' ; g = 13 ; h = 'pro'
+print(f+' '+str(g)+' '+h)
+
+i = 12
+print(float(i))
+print(str(i))
+
+j= 23.45
+print(int(j))
+print(str(j))
+
+k='23'
+print(int(k))
+print(float(k))
+
+# l='ew'
+# print(int(l)) Value error
+# print(float(l)) Value error
